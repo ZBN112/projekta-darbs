f29efa181d0571eb22575d7f383bea79027851fd
ZBN112 <147724620+ZBN112@users.noreply.github.com>
2023-11-02 22:17:27 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const reactionButton = document.getElementById("reactionButton");
+const reactionTimeDisplay = document.getElementById("reactionTime");
+let startTime, endTime, timeoutId;
+
+function startReactionTimeTest() {
+  reactionButton.style.backgroundColor = "red";
+  reactionButton.disabled = true;
+
+
+  const delay = getRandomDelay(1000, 5000);
+  timeoutId = setTimeout(changeButtonColor, delay);
+}
+
+function changeButtonColor() {
+  reactionButton.style.backgroundColor = "green";
+  startTime = new Date().getTime();
+  reactionButton.disabled = false;
+
+  reactionButton.onclick = function () {
+    endTime = new Date().getTime();
+    const reactionTime = endTime - startTime;
+    reactionTimeDisplay.textContent = `Tavs reakcijas laiks : ${reactionTime} ms`;
+    clearTimeout(timeoutId); 
+    reactionButton.style.backgroundColor = "red";
+    reactionButton.disabled = true;
+    reactionButton.onclick = null;
+  };
+}
+
+function getRandomDelay(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
